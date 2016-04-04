7a3f3cccff33c498e11b98fb2fb7d78d60a256dd
akram045 <akram045@mail.fresnostate.edu>
2016-04-04 12:51:01 -0700

    Project First
    Project application

---
+++ 16_Project/Step1-WebApplication/main.go
@@ -0,0 +1,31 @@
+
+/*
+create a web application that serves an HTML template.
+ */
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"log"
+)
+
+
+//Serves index.html
+func serve_the_webpage(res http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles("login.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.Execute(res, nil)
+}
+
+
+func main() {
+	http.HandleFunc("/", serve_the_webpage)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	log.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ 16_Project/Step2/main.go
@@ -0,0 +1,65 @@
+/*
+
+have the application write a cookie called "session-fino" with a UUID. The cookie should serve HttpOnly and
+					you should have the "Secure" flag set also though comment the "Secure" flag out as we're not using http
+ */
+
+package main
+
+import (
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func login(res http.ResponseWriter, req *http.Request) {
+
+	createCookie(&res)
+
+	temp, err := template.ParseFiles("login.html")
+	// Logging possible errors
+	logFatalError(err)
+
+	temp.Execute(res, nil)
+	// Logging possible errors
+	logFatalError(err)
+}
+
+func createCookie(res *http.ResponseWriter) {
+	// Generating a new ID
+	id, err := uuid.NewV4()
+	// Logging possible errors
+	logFatalError(err)
+
+	// Setting the cookie
+	cookie := &http.Cookie{
+		Name:     "session-fino",
+		Value:    id.String(),
+		//		Secure: true,
+		HttpOnly: true,
+	}
+
+	// Setting the cookie on the response back to the client
+	http.SetCookie(*res, cookie)
+}
+
+func main() {
+
+	// Ignoring favicon.ico
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	// Setting the handler for login
+	http.HandleFunc("/", login)
+
+	// Setting the listener on port 8080
+	log.Println("Listening to 8080 ...")
+	http.ListenAndServe(":8080", nil)
+}
+
+// Logs error at Fatal level given the error is not nil.
+func logFatalError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
